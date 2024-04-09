7abf77b01b2d7669ca4e14453c8cea7e33e98828
mahidhar reddy <mahidhar100008@gmail.com>
2024-04-09 12:01:55 +0530

    setup prisma as connection pool and added db links in env(original) and wrangeler.toml(prisma)

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,4 +7,23 @@ app.get('/', (c) => {
 })
 
 
+app.post("/api/v1/signup" , (c) => {
+ return c.text("signup");
+})
+
+app.post("/api/v1/signin" , (c) => {
+ return c.text("signup");
+})
+
+app.post("/api/v1/blog" , (c) => {
+ return c.text("signup");
+})
+
+app.put("/api/v1/blog" , (c) => {
+ return c.text("signup");
+})
+
+app.get("/api/v1/blog/:id" , (c) => {
+ return c.text("signup");
+})
 export default app

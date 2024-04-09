3ec3942978382b203f77e7b045422e841f387333
mahidhar reddy <mahidhar100008@gmail.com>
2024-04-09 12:24:39 +0530

    initialized the projetn fully

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,33 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings:{
+    DATABASE_URL : string
+  }
+}>()
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
 
-app.post("/api/v1/signup" , (c) => {
- return c.text("signup");
+app.post("/api/v1/signup" , async (c) => {
+  
+const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+}).$extends(withAccelerate())
+
+
+const body = await c.req.json();
+await prisma.user.create({
+  data: {
+    email: body.email,
+    password: body.password
+  }
+})
+ return c.text("signup sucessfully");
 })
 
 app.post("/api/v1/signin" , (c) => {
